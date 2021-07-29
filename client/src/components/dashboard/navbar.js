2c39d4fedc13a88547d25e992ab42d31942e1e6e
Kaushik Kumar Bora <kaushikkumarbora@gmail.com>
2021-07-29 13:49:31 +0530

    Fixed Name

--- client/src/components/dashboard/navbar.js
+++ client/src/components/dashboard/navbar.js
@@ -5,13 +5,13 @@ function NavBarTop() {
         // <!-- Navigation-->
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div className="container px-4 px-lg-5">
-                <div className="navbar-brand" >The Big Picture</div>
+                <div className="navbar-brand" >Netherite</div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
                     aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
                         className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active"><div className="nav-link">Ligma</div></li>
+                        <li className="nav-item active"><div className="nav-link">Team - BitOverflow - Rabijit Singh, Subhasish Goswami and Kaushik Kumar Bora</div></li>
                     </ul>
                 </div>
             </div>
